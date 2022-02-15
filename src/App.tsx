4a6e113251ced2548244cef239cb8a5801c090b6
zeronedev <ok7375@gmail.com>
2022-02-15 11:38:26 +0900

    #7.2 Basic Animations: transition, initial, animate

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -8,7 +9,7 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
@@ -19,7 +20,12 @@ const Box = styled.div`
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box
+        transition={{ type: "spring", delay: 0.5 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+      />
+      <motion.div></motion.div>
     </Wrapper>
   );
 }
